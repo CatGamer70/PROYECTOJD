7e681533a876a29013ea51cc75d298ce6778609b
CatGamer70 <erj703343@gmail.com>
2024-10-03 09:42:06 -0600

    Se agregan las carpetas y archivos del proyecto

---
+++ Cliente/Vistas/pie.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  </head>
+  <body>
+    <div class="container" style="text-align: center; padding-top: 40px; background-color:#e7dee8;">
+      <div class="row">
+        <div class="col">
+          <pre style="padding-top: 50px;">
+            Domicilio: Emiliano Zapato #5
+            San Francisco Tepeyecac, San Martin Tex, Puebla
+            Telefono: 2482151988
+            Representante: AVACE
+
+          </pre>
+        </div>
+        <div class="col">
+          <img src="Recursos/Logo.png" width="100px" height="100px" style="padding-top: 5px;">
+        </div>
+        <div class="col">
+         <iframe 
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3089.8070213609053!2d-98.43947329592717!3d19.247360941151033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfd3a7409068f9%3A0x6cafe0e277fe1fc!2sCalle%20Emiliano%20Zapata%2C%2074129%20San%20Francisco%20Tepeyecac%2C%20Pue.!5e0!3m2!1ses-419!2smx!4v1724942065984!5m2!1ses-419!2smx" 
+         width="300" 
+         height="250" 
+         style="border:0;" 
+         allowfullscreen="" 
+         loading="lazy" 
+         referrerpolicy="no-referrer-when-downgrade">
+         </iframe>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
